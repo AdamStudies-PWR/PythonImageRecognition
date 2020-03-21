cccf7a908c5e32f438a20cb35331bfa7072b9870
ApocalypseArisen <adam98552@gmail.com>
2020-03-21 17:53:50 +0100

    Ogarniecie podstaw

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 
-from tensorflow.keras.models import Seqential
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
-DB = "/run/media/adam/DLinux/ripo/Croped/"
-# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
+IMG_HEIGHT = 200
+IMG_WIDTH = 200
 
-print("Zanleziono ", DB, " obrazów do treningu")
+if len(sys.argv) != 2:
+    print("Nipoprawana ilość argumentów")
+    exit(0)
+
+# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
+DB = sys.argv[1]
+# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
+if os.path.isdir(DB):
+    images = len(os.listdir(DB))
+    if images == 0:
+        print("Brak obrazów! Sprawdź czy podano poprawną ścieżkę!")
+        exit(0)
+    else:
+        print("Zanleziono ", images, " obrazów do treningu")
+else:
+    print("Niepoprawna ścieżka! Spróbuj ponownie")
+    exit(0)
