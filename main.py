fcd8fcfe30cb876501c470c6b89a32adff3b4cea
ApocalypseArisen <adam98552@gmail.com>
2020-03-21 19:05:20 +0100

    Podział plików

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if len(sys.argv) != 2:
 # TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
 DB = sys.argv[1]
 # TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
-if os.path.isdir(DB):
+if os.path.exists(DB):
     images = len(os.listdir(DB))
     if images == 0:
         print("Brak obrazów! Sprawdź czy podano poprawną ścieżkę!")
@@ -31,3 +31,15 @@ if os.path.isdir(DB):
 else:
     print("Niepoprawna ścieżka! Spróbuj ponownie")
     exit(0)
+
+males = DB + "male"
+females = DB + "female"
+
+if not os.path.exists(males): os.mkdir(males)
+if not os.path.exists(females): os.mkdir(females)
+
+for filename in os.listdir(DB):
+    if not filename.find('g1') == -1:
+        os.replace(DB + filename, males + "/" + filename)
+    else:
+        os.replace(DB + filename, females + "/" + filename)
