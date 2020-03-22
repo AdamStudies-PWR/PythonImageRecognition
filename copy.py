57028cb7be942d823f0c566ccc0569e9ce014674
ApocalypseArisen <adam98552@gmail.com>
2020-03-22 17:45:17 +0100

    Wykorzystanie przykłądowego algorytmu do treningu

---
+++ copy.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Nipoprawana ilość argumentów")
+    exit(0)
+
+# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
+DB = sys.argv[1]
+# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
+if os.path.exists(DB):
+    images = len(os.listdir(DB))
+    if images == 0:
+        print("Brak obrazów! Sprawdź czy podano poprawną ścieżkę!")
+        exit(0)
+    else:
+        print("Zanleziono ", images, " obrazów do treningu")
+else:
+    print("Niepoprawna ścieżka! Spróbuj ponownie")
+    exit(0)
+
+males = DB + "male"
+females = DB + "female"
+
+if not os.path.exists(males): os.mkdir(males)
+if not os.path.exists(females): os.mkdir(females)
+
+try:
+    for filename in os.listdir(DB):
+        if filename != "male" and filename != "female":
+            if not filename.find('g1') == -1:
+                os.replace(DB + filename, males + "/" + filename)
+            else:
+                os.replace(DB + filename, females + "/" + filename)
+except:
+    print("Ding Dong somethings wrong!")
+    print(DB)
+    print(females)
+    print(males)
--- main.py
+++ main.py
@@ -1,39 +1,125 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
 
-if len(sys.argv) != 2:
-    print("Nipoprawana ilość argumentów")
-    exit(0)
+PATH = sys.argv[1]
 
-# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
-DB = sys.argv[1]
-# TU WSTAWIC SCIEZKE DO OBRAZÓW TRENINGOWYCH
-if os.path.exists(DB):
-    images = len(os.listdir(DB))
-    if images == 0:
-        print("Brak obrazów! Sprawdź czy podano poprawną ścieżkę!")
-        exit(0)
-    else:
-        print("Zanleziono ", images, " obrazów do treningu")
-else:
+if not os.path.exists(PATH):
     print("Niepoprawna ścieżka! Spróbuj ponownie")
     exit(0)
 
-males = DB + "male"
-females = DB + "female"
+train_dir = os.path.join(PATH, "train")
+validation_dir = os.path.join(PATH, "validation")
 
-if not os.path.exists(males): os.mkdir(males)
-if not os.path.exists(females): os.mkdir(females)
+train_male_dir = os.path.join(train_dir, 'male')  # directory with our training cat pictures
+train_female_dir = os.path.join(train_dir, 'female')  # directory with our training dog pictures
+validation_male_dir = os.path.join(validation_dir, 'male')  # directory with our validation cat pictures
+validation_female_dir = os.path.join(validation_dir, 'female')  # directory with our validation dog pictures
 
-try:
-    for filename in os.listdir(DB):
-        if filename != "male" and filename != "female":
-            if not filename.find('g1') == -1:
-                os.replace(DB + filename, males + "/" + filename)
-            else:
-                os.replace(DB + filename, females + "/" + filename)
-except:
-    print("Ding Dong somethings wrong!")
-    print(DB)
-    print(females)
-    print(males)
+num_male_tr = len(os.listdir(train_male_dir))
+num_female_tr = len(os.listdir(train_female_dir))
+
+num_male_val = len(os.listdir(validation_male_dir))
+num_female_val = len(os.listdir(validation_female_dir))
+
+total_train = num_male_tr + num_female_tr
+total_val = num_male_val + num_female_val
+
+print('Obrazki treningowe mężczyźni:', num_male_tr)
+print('Obrazki treningowe kobiety:', num_female_tr)
+
+print('Obrazki sprawdzające mężczyźni:', num_male_val)
+print('Obrazki sprawdzające kobiety:', num_female_val)
+print("--")
+print("W sumie treningowych:", total_train)
+print("W sumie testowych:", total_val)
+
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=validation_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+
+sample_training_images, _ = next(train_data_gen)
+
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+plotImages(sample_training_images[:5])
+
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    MaxPooling2D(),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=total_train // batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=total_val // batch_size
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
