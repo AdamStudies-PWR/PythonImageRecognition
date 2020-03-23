c19976fbb0437db966216307b658875278849c95
Filip <filip.k.wanat@gmail.com>
2020-03-23 16:00:13 +0100

    Add checkpoints and saving model

--- main.py
+++ main.py
@@ -20,10 +20,14 @@ if not os.path.exists(PATH):
 train_dir = os.path.join(PATH, "train")
 validation_dir = os.path.join(PATH, "validation")
 
-train_male_dir = os.path.join(train_dir, 'male')  # directory with our training cat pictures
-train_female_dir = os.path.join(train_dir, 'female')  # directory with our training dog pictures
-validation_male_dir = os.path.join(validation_dir, 'male')  # directory with our validation cat pictures
-validation_female_dir = os.path.join(validation_dir, 'female')  # directory with our validation dog pictures
+# directory with our training cat pictures
+train_male_dir = os.path.join(train_dir, 'male')
+# directory with our training dog pictures
+train_female_dir = os.path.join(train_dir, 'female')
+# directory with our validation cat pictures
+validation_male_dir = os.path.join(validation_dir, 'male')
+# directory with our validation dog pictures
+validation_female_dir = os.path.join(validation_dir, 'female')
 
 num_male_tr = len(os.listdir(train_male_dir))
 num_female_tr = len(os.listdir(train_female_dir))
@@ -54,30 +58,36 @@ validation_image_generator = ImageDataGenerator(rescale=1./255)
 train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
                                                            directory=train_dir,
                                                            shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           target_size=(
+                                                               IMG_HEIGHT, IMG_WIDTH),
                                                            class_mode='binary')
 
 val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
                                                               directory=validation_dir,
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              target_size=(
+                                                                  IMG_HEIGHT, IMG_WIDTH),
                                                               class_mode='binary')
 
 sample_training_images, _ = next(train_data_gen)
 
 # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+
+
 def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
     axes = axes.flatten()
-    for img, ax in zip( images_arr, axes):
+    for img, ax in zip(images_arr, axes):
         ax.imshow(img)
         ax.axis('off')
     plt.tight_layout()
     plt.show()
 
-plotImages(sample_training_images[:5])
+
+# plotImages(sample_training_images[:5])
 
 model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    Conv2D(16, 3, padding='same', activation='relu',
+           input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
     MaxPooling2D(),
     Conv2D(32, 3, padding='same', activation='relu'),
     MaxPooling2D(),
@@ -94,19 +104,44 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-history = model.fit_generator(
+# Saving progress
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 save_best_only=True,
+                                                 verbose=1)
+
+# load
+
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+
+model.load_weights(latest)
+
+print("Restoring model")
+loss, acc = model.evaluate(train_data_gen, verbose=1)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+history = model.fit(
     train_data_gen,
     steps_per_epoch=total_train // batch_size,
     epochs=epochs,
     validation_data=val_data_gen,
-    validation_steps=total_val // batch_size
-)
+    validation_steps=total_val // batch_size,
+    callbacks=[cp_callback])  # Pass callback to training
+
+# tf.saved_model.save(model, 'saved_model/genders-3')
+model.save('saved_model/genders')
+print("Saved model")
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
-loss=history.history['loss']
-val_loss=history.history['val_loss']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
