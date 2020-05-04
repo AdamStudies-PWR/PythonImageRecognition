38857ddf6f4c3e486f38ff96197e08737155d044
Adam Krizar <adam.krizar@gmail.com>
2020-05-04 18:13:18 +0200

    Fixed file operations

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import cv2 as cv
+import shutil as sh
 import tensorflow as tf
 
 from crop_face import crop_faces
@@ -55,49 +56,49 @@ if ch == "1":
     if not os.path.exists(PATH + "/train/male2"):
         os.mkdir(PATH + "/train/male2")
     just_crop(PATH + "/train/male", PATH + "/train/male2")
-    os.rmdir(PATH + "/train/male")
+    sh.rmtree(PATH + "/train/male")
     os.replace(PATH + "/train/male2", PATH + "/train/male")
     
     if not os.path.exists(PATH + "/train/female2"):
         os.mkdir(PATH + "/train/female2")
     just_crop(PATH + "/train/female", PATH + "/train/female2")
-    os.rmdir(PATH + "/train/female")
+    sh.rmtree(PATH + "/train/female")
     os.replace(PATH + "/train/female2", PATH + "/fetrain/male")
 
     if not os.path.exists(PATH + "/validation/male2"):
         os.mkdir(PATH + "/validation/male2")
     just_crop(PATH + "/validation/male", PATH + "/validation/male2")
-    os.rmdir(PATH + "/validation/male")
+    sh.rmtree(PATH + "/validation/male")
     os.replace(PATH + "/validation/male2", PATH + "/validation/male")
 
     if not os.path.exists(PATH + "/validation/female2"):
         os.mkdir(PATH + "/validation/female2")
     just_crop(PATH + "/validation/female", PATH + "/validation/female2")
-    os.rmdir(PATH + "/validation/female")
+    sh.rmtree(PATH + "/validation/female")
     os.replace(PATH + "/validation/female2", PATH + "/fevalidation/male")
 elif ch == "2":
     if not os.path.exists(PATH + "/train/male2"):
         os.mkdir(PATH + "/train/male2")
     double_data(PATH + "/train/male", PATH + "/train/male2")
-    os.rmdir(PATH + "/train/male")
+    sh.rmtree(PATH + "/train/male")
     os.replace(PATH + "/train/male2", PATH + "/train/male")
 
     if not os.path.exists(PATH + "/train/female2"):
         os.mkdir(PATH + "/train/female2")
     double_data(PATH + "/train/female", PATH + "/train/female2")
-    os.rmdir(PATH + "/train/female")
+    sh.rmtree(PATH + "/train/female")
     os.replace(PATH + "/train/female2", PATH + "/fetrain/male")
 
     if not os.path.exists(PATH + "/validation/male2"):
         os.mkdir(PATH + "/validation/male2")
     double_data(PATH + "/validation/male", PATH + "/validation/male2")
-    os.rmdir(PATH + "/validation/male")
+    sh.rmtree(PATH + "/validation/male")
     os.replace(PATH + "/validation/male2", PATH + "/validation/male")
 
     if not os.path.exists(PATH + "/validation/female2"):
         os.mkdir(PATH + "/validation/female2")
     double_data(PATH + "/validation/female", PATH + "/validation/female2")
-    os.rmdir(PATH + "/validation/female")
+    sh.rmtree(PATH + "/validation/female")
     os.replace(PATH + "/validation/female2", PATH + "/fevalidation/male")
 
 
